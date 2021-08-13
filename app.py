cf87cfed0110f1ed20a21dcf80f3ef52112ab0d8
Aw Khai Loong <sky99aw@gmail.com>
2021-08-13 23:37:46 +0800

    Update prediction

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ from flask import Flask, request, jsonify
 from predict import model_predict
 
 app = Flask(__name__)
-app.run()
 
 @app.route('/')
 def hello_world():
-    return 'Hello, this is lit-flask@@@!'
+    return 'Hello, edited this is lit-flask@@@!'
 
 @app.route('/predict_text', methods=['POST'])
 def predict_text():
@@ -15,3 +14,6 @@ def predict_text():
     category_list = model_predict(text)
 
     return str(category_list)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=3000, threaded=True, debug=True)
--- predict.py
+++ predict.py
@@ -3,26 +3,40 @@ import tensorflow as tf
 import pickle
 import tensorflow
 from tensorflow import keras
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+import re
+from nltk.stem.porter import PorterStemmer
+ps = PorterStemmer()
 
 model = keras.models.load_model('saved_model')
 with open('tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
 
-sequence_len = 15
+Y_columns = ['Contract', 'Criminal', 'Family', 'Property', 'Tort_1', 'Tort_2']
+Y_columns = sorted(Y_columns)
 
-Y_columns = sorted(['Corporate', 'Criminal', 'Divorce', 'Legal_Procedures'])
-map_idx_to_category = { i: Y_columns[i] for i in range(len(Y_columns)) }
+ps = PorterStemmer()
+sequence_len = 20
 
+def model_predict(text):
+    print('@ Sentence input:', text)
 
-def model_predict(sentence):
-    print('@ Sentence input:', sentence)
-    sequence = tokenizer.texts_to_sequences( [sentence] )[0]
+    text = re.sub('[^a-zA-Z]', ' ', text)
+    text = text.lower()
+    text = text.split()
+    text = [ps.stem(word) for word in text]
+    text = ' '.join(text)
+    
+    print(text)
+    sequence = tokenizer.texts_to_sequences( [text] )[0]
+
+    if (len(sequence) == 0):
+        return Y_columns
+
     if len(sequence) < sequence_len:
         # sequence = [0] * (sequence_len - len(sequence)) + sequence
         num_repeats = (sequence_len // len(sequence)) + 1
         sequence = sequence * num_repeats
-    print('@ sequence:', sequence)
+#     print('@ sequence:', sequence)
 
     sequences = []
     num_sequences = len(sequence) // sequence_len
@@ -35,13 +49,48 @@ def model_predict(sentence):
     y_prob = model.predict(sequences)
     y_avg_prob = list(np.average(y_prob, axis=0))
 
-    y_avg_prob_map = { Y_columns[i] : y_avg_prob[i] for i in range(len(y_avg_prob)) if y_avg_prob[i] >= 0.1 }
-    print('Map:', y_avg_prob_map)
+    y_avg_prob_map = { Y_columns[i] : y_avg_prob[i] for i in range(len(y_avg_prob)) }
+#     print('Map:', y_avg_prob_map)
 
     y_avg_prob_map_sorted = {k: v for k, v in sorted(y_avg_prob_map.items(), key=lambda x: x[1], reverse=True)}
     print('Sorted Map:', y_avg_prob_map_sorted)
     
-    category_list = list(y_avg_prob_map_sorted.keys())
-    print('Category list: ', category_list)
+    category_list = list(y_avg_prob_map_sorted.keys())[0:2]
+#     print('Category list: ', category_list)
 
     return category_list
+
+test_sentences = [
+    'Divorcing my wife',
+    'Abused by my husband',
+    'I want to create a will',
+    'Mental health dropping',
+    'I wish to adopt a child',
+    
+    'I have an employment issue',
+    'Want to Claim insurance',
+    'I took out a large loan',
+    'I need some help on an investment I made',
+    'I bought a product and need to use lemon law',
+    
+    'I intend to buy a BTO house',
+    'paying my mortgage',
+    'I had a huge fight with my tenant. I am not sure what I should do. He keeps screaming at me and does not reply me',
+    'I got sued for a copyright',
+    'I had a will estate planning issue',
+    
+    'I was harassed by some stranger on the street',
+    'I received a defamation letter',
+    'I have issues with privacy',
+    'PDPA is a serious concern my company faces',
+    'My neighbor keeps banging my door',
+    
+    'I wish to make a personal injury claim',
+    'I was threatened by the man earlier',
+    'My employer neglected my well-being',
+    'I got into a traffic accident',
+    'My doctor did not prescribe the right medicine',
+]
+
+for sentence in test_sentences:
+    model_predict(sentence)
