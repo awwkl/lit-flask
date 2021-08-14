0c61a05fd5f6146b10cd097413520740d4d55806
Aw Khai Loong <sky99aw@gmail.com>
2021-08-14 13:27:30 +0800

    Edit response of text prediction

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def hello_world():
 def predict_text():
     text = request.args.get('text')
     category_list = model_predict(text)
-    return str(category_list)
+    return str(','.join(category_list))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000, threaded=True, debug=True)
